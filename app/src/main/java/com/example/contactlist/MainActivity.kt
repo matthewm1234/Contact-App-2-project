80603b4051f85010cb6c88ceb50dca8fbad706e8
Matthewm1234 <mondaym12@yahoo.com>
2021-05-16 23:09:42 +0100

    Commit to save initial work

---
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.contactlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.contactlist.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private val myAdapter = ContactAdapter()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        setupData(binding)
+    }
+    private fun setupData (binding: ActivityMainBinding){
+
+        binding.contactRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = myAdapter
+        }
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.add_contact_dialog,null)
+        builder.setView(view)
+
+        val name = view.findViewById<TextView>(R.id.nameEt)
+        val no = view.findViewById<TextView>(R.id.numberEt)
+        val saveBtn = view.findViewById<TextView>(R.id.saveBt)
+
+        no.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                saveBtn.isEnabled = s?.length == 11
+            }
+
+        })
+        val alertDialog = builder.create()
+        saveBtn.setOnClickListener{
+            val contact = Contact(name.text.toString(), no.text.toString())
+            val contacts = mutableListOf(contact)
+            myAdapter.setupContacts(contacts)
+            alertDialog.dismiss()
+        }
+
+        binding.fab.setOnClickListener {
+                alertDialog.show()
+        }
+    }
+}
